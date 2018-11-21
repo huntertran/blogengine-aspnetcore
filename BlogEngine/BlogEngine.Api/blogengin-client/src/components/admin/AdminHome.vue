46f89543a45cb8f2742f6118f2da65e5abbb7e4f
hunter.tran <hunter.tran.92@outlook.com>
2018-11-21 11:39:09 +0700

    move to new folder as old folder error

---
+++ BlogEngine/BlogEngine.Api/blogengin-client/src/components/admin/AdminHome.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-layout text-xs-center wrap>
+        <v-flex xs6>
+            <h1>Total Post: {{totalPost}}</h1>
+        </v-flex>
+        <v-flex xs6>
+            <h1>Total Category: {{totalCategory}}</h1>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      totalPost: 5,
+      totalCategory: 5
+    };
+  }
+};
+</script>
---
+++ BlogEngine/BlogEngine.Api/blogengin-client/src/components/posts/ReadPost.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-container>
+      <v-layout>
+        <h1>{{post.title}}</h1>
+      </v-layout>
+        <v-layout>
+            <v-flex xs12 v-html="post.content"></v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      post: {
+        title: "",
+        summary: "",
+        content: ""
+      }
+    };
+  },
+  methods: {
+    getPost: function(postId) {
+      var _this = this;
+      Axios.get("/posts/find?id=" + postId).then(function(response) {
+        _this.post = response.data;
+      });
+    }
+  },
+  mounted() {
+    this.getPost(this.$route.params.postId);
+    // registerBrush(CSharpBrush);
+    // SyntaxHighlighter.highlight({
+    //   gutter: false
+    // });
+  }
+};
+</script>
