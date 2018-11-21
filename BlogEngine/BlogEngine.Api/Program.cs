7be298c63a769532c0555fe8a8dbbdba7f343997
hunter.tran <hunter.tran.92@outlook.com>
2018-11-21 15:08:09 +0700

    remove redundant

--- BlogEngine/BlogEngine.Api/Program.cs
+++ BlogEngine/BlogEngine.Api/Program.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
-namespace BlogEngine.Api
+﻿namespace BlogEngine.Api
 {
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
     public class Program
     {
         public static void Main(string[] args)
